31fd143d34e78b1b57da0c5e2f09cf5e7701c0fa
meheraz1100 <meheraz1100@gmail.com>
2024-06-04 15:49:30 +0600

    project init

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Footer from "../../Shared/Footer/Footer";
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className="text-xl">This is home</h1>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
